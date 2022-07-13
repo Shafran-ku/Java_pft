fe61c2ec2e4b5374ea9058d666f9224e53801b5e
Shafran-ku <83473732+Shafran-ku@users.noreply.github.com>
2022-07-13 23:34:13 +0300

    Добавили возможность выбирать браузер для запуска тестов  (Firefox, Chrome, IE)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
@@ -7,7 +9,7 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 //перед каждым тестовым методом инициализирует объект типа ApplicationManager, после того как метод отработал - разрушает его
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);    //выбор нужного браузера IE CHROME FIREFOX
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
