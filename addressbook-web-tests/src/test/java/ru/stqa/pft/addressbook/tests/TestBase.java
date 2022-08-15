49996c2ff86fc285bb870faf4aaa848fbe3a4990
Shafran-ku <83473732+Shafran-ku@users.noreply.github.com>
2022-08-15 22:11:16 +0300

    5.1 Браузер запускается 1 раз для всех тестов (заменили аннотации в методах setUp и tearDown на @BeforeSuite и @AfterSuite соответственно (вместо @Beforemethod и  @Aftermethod); в TestBase переменную app сделали static, теперь все классы могут пользоваться глобальной переменной app)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -2,21 +2,21 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 //перед каждым тестовым методом инициализирует объект типа ApplicationManager, после того как метод отработал - разрушает его
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);    //выбор нужного браузера IE CHROME FIREFOX
+    protected static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);    //выбор нужного браузера IE CHROME FIREFOX
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeSuite //Suite может состоять из нескольких тестов
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterSuite
     public void tearDown() throws Exception {
         app.stop();
     }
