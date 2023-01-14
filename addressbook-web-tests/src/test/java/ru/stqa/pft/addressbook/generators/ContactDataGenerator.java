bc0a4808d32d824aae57348cae6e5060e9ee562e
Shafran-ku <83473732+Shafran-ku@users.noreply.github.com>
2023-01-14 15:52:20 +0300

    HW13: +Параметризация тестов для контактов (JSON)
    чтение ТД в ContactCreation с помощью DataProvider

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -69,7 +69,7 @@ public class ContactDataGenerator {
                     .withLastname(String.format("lastname %s", i))
                     .withAddress(String.format("address %s", i))
                     .withEmail(String.format("email %s", i))
-                    .withHomePhone(String.format("homePhone %s", i))
+                    .withHomePhone(String.format("%s%s%s-%s%s%s", i,i,i,i,i,i))
                     .withGroup(String.format("group %s", i)));
         }
         return contacts;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,17 +1,54 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
+
+    @DataProvider //для JSON
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        //ридер для чтения данных
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+
+        //десериализация формата json: читаем содержимое файла в переменную, потом ее обрабатываем
+        String json = "";
+
+        //читаем строки
+        String line = reader.readLine();
+
+        //для чтения всех строк файла делаем цикл
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        //новый объект
+        Gson gson = new Gson();
+        //TypeToken<List<ContactData>>(){}.getType() - для обработки типа данных, заключенных в <> (<ContactData>)
+        List<ContactData> groups = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+
+        //к каждому объекту нужно применить функцию, которая этот объект завернет в массив
+        //collectors из потока собирает список
+        //и у этого cписка берем итератор, который возвращаем
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+
     //предусловия теста
     @BeforeMethod
     public void ensurePreconditions() {
@@ -23,8 +60,10 @@ public class ContactCreationTests extends TestBase {
         app.goTo().HomePage();
     }
 
-    @Test
-    public void testContactCreation() throws Exception {
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact) {
+        //Основной тест
 
         //множество элементов до добавления
         Contacts before = app.contact().all();
@@ -32,9 +71,12 @@ public class ContactCreationTests extends TestBase {
         //переменная для картинки
         File photo= new File("src/test/resources/stru.png");
 
-        //сделали переменную
+        /*
+        //старое создание контакта с фото
         ContactData contact = new ContactData().withFirstname("Den").withLastname("Kh.").withAddress("Suvorova st.")
                 .withEmail("den@mail.ru").withHomePhone("+79188888777").withGroup("test1").withPhoto(photo);
+         */
+
         app.contact().create(contact);
 
         //сравнение размера до и после добавления
