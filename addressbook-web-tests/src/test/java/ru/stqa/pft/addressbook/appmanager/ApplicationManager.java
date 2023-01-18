cba616424c12ec56ee3a8d53cb2e22b99f9c51f9
Shafran-ku <83473732+Shafran-ku@users.noreply.github.com>
2023-01-18 09:45:57 +0300

    6.10 Описание конфигурации тестового стенда
    -файлы свойств для конфигурации
    -системные свойства (передаются при запуске тестов)
    	-тип браузера
    	-свойство target
    
    Cвойство target – в качестве параметра передаем имя конфигурационного файла, в котором хранится инф-ия о тестируемой системе, например local (тогда инф-ия будет загружена из ../resources/local/properties)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,9 +6,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private final Properties properties;
     WebDriver wd;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
@@ -16,11 +22,20 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser) throws IOException {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        //загрузка свойств из конфигурационного файла (local.properties)
+
+        //local - название конфигур-го файла
+        String target = System.getProperty("target", "local");
+
+        //вместо %s будет подставлен target
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -29,12 +44,17 @@ public class ApplicationManager {
             wd = new InternetExplorerDriver();
         }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+
+        //использование загруженных свойств из конфигурационного файла (local.properties)
+        wd.get(properties.getProperty("web.baseUrl"));
+
         contactHelper = new ContactHelper(wd);
         groupHelper = new GroupHelper(contactHelper.wd);
         navigationHelper = new NavigationHelper(contactHelper.wd);
         sessionHelper = new SessionHelper(contactHelper.wd);
-        sessionHelper.login("admin", "secret");
+
+        //использование загруженных свойств из конфигурационного файла (local.properties)
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -6,10 +6,21 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 //перед каждым тестовым методом инициализирует объект типа ApplicationManager, после того как метод отработал - разрушает его
 public class TestBase {
 
-    protected static ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);    //выбор нужного браузера IE CHROME FIREFOX
+    //указали системное свойство тип браузера - он указывается в конфиге; если там не указан то по-умолчанию CHROME (BrowserType)
+    protected static final ApplicationManager app;    //выбор нужного браузера IE CHROME FIREFOX
+
+    static {
+        try {
+            app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @BeforeSuite //глобальная инициализация. (Suite может состоять из нескольких тестов)
     public void setUp() throws Exception {
